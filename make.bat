a71988d63850f89e73ba7f95272a25daf8ef0181
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-10-28 17:11:15 -0300

    Adicionando arquivo para compilar e linkar

---
+++ make.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set MAIN_FILE=main.asm
+set OUT_FILE=jogo.exe
+set LIBPATH=lib
+set DEBUG=1
+
+set OBJ=out/%MAIN_FILE:.asm=.obj%
+set LST=out/%MAIN_FILE:.asm=.lst%
+set OUT=bin/%OUT_FILE%
+
+:prepare
+    if %DEBUG%==1 (
+        set MLFLAGS=/nologo /c /Sf /Fl%LST% /coff /Sn /Zd /Zi /I %LIBPATH% 
+        set LINKFLAGS=/DEBUG /DEBUGTYPE:COFF /NOLOGO /LIBPATH:%LIBPATH% /SUBSYSTEM:CONSOLE
+    ) else (
+        set MLFLAGS=/nologo /c /Fl /coff /Sn /I %LIBPATH%
+        set LINKFLAGS=/NOLOGO /LIBPATH:%LIBPATH% /SUBSYSTEM:CONSOLE
+    )
+
+:create_directory
+    if not exist "bin" (
+        mkdir bin
+    )
+
+    if not exist "out" (
+        mkdir out
+    )
+
+:compile
+    ml %MLFLAGS% /Fo %OBJ% %MAIN_FILE%
+
+:link
+    @echo  Linking: %OBJ%
+    link %LINKFLAGS% %OBJ% /OUT:%OUT%
