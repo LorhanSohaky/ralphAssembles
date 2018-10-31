c7756556eb715beb978744089a7cf73a927f6021
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-10-31 13:55:37 -0300

    Adicionando função para salvar configurações antes de iniciar o jogo e função para restaurar configurações após término do jogo

--- main.asm
+++ main.asm
@@ -6,13 +6,19 @@ INCLUDELIB kernel32.lib
 
 INCLUDE example.asm
 
+corPadrao TEXTEQU %70h                                              ; Cores do jogo
+
 .DATA
+    corAnterior BYTE ?                                              ; Cores do console antes de iniciar o jogo
+    barraDeTitulo BYTE "Ralph Assembles",0                          ; Título da janela
     msg BYTE "Hello World!",0
     msg2 BYTE "Hello World 2!",0
 
 .CODE
 
 main PROC
+    call inicializar
+
     MOV ECX, LENGTHOF msg
     MOV ESI, OFFSET msg
 
@@ -32,7 +38,49 @@ main PROC
     mov edx, OFFSET msg2
     call exemplo
 
+    call finalizar
+
     exit
 main ENDP
+
+;---------------------------------------------------------
+;                        inicializar PROC
+;   Guarda as cores do console antes de iniciado o jogo e
+; coloca o nome do jogo no título da janela.
+; Entrada: Nada
+; Saída: Nada
+; Requer: Nada
+;---------------------------------------------------------
+inicializar PROC
+
+    call GetTextColor
+    mov corAnterior, al
+
+    mov eax, corPadrao
+    call SetTextColor
+
+
+
+    push OFFSET barraDeTitulo
+    call SetConsoleTitle
+
+    ret
+
+inicializar ENDP
+
+;---------------------------------------------------------
+;                       finalizar PROC
+;   Restaura a cor padrão do console.  
+; Entrada: Nada
+; Saída: Nada
+; Requer: Chamar inicializar
+;---------------------------------------------------------
+
+finalizar PROC
+    movzx eax, corAnterior
+    call SetTextColor
+    
+    ret
+finalizar ENDP
 
 END main
