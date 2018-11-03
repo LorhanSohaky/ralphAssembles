852444a5b077f83bce647d7ceb37d3919b2489ea
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-11-02 23:16:15 -0300

    Mudando a codificação de caracteres

--- main.asm
+++ main.asm
@@ -10,9 +10,9 @@ CORPADRAO TEXTEQU %70h                                              ; Cores do j
 
 .DATA
     corAnterior BYTE ?                                              ; Cores do console antes de iniciar o jogo
-    barraDeTitulo BYTE "Ralph Assembles",0                          ; T√≠tulo da janela
+    barraDeTitulo BYTE "Ralph Assembles",0                          ; T°tulo da janela
     msg BYTE "Hello World!",0
-    msg2 BYTE "Hello World 2!",0
+    msg2 BYTE "Hello World2!",0
 
 .CODE
 
@@ -47,9 +47,9 @@ main ENDP
 ;---------------------------------------------------------
 ;                        inicializar PROC
 ;   Guarda as cores do console antes de iniciado o jogo e
-; coloca o nome do jogo no t√≠tulo da janela.
+; coloca o nome do jogo no t°tulo da janela.
 ; Entrada: Nada
-; Sa√≠da: Nada
+; Sa°da: Nada
 ; Requer: Nada
 ;---------------------------------------------------------
 inicializar PROC
@@ -72,9 +72,9 @@ inicializar ENDP
 
 ;---------------------------------------------------------
 ;                       finalizar PROC
-;   Restaura a cor padr√£o do console.  
+;   Restaura a cor padrÑo do console.  
 ; Entrada: Nada
-; Sa√≠da: Nada
+; Sa°da: Nada
 ; Requer: Chamar inicializar
 ;---------------------------------------------------------
 finalizar PROC
