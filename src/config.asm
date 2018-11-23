1b336c54fb9e477e7398e99c500d4657cc11b3cc
Lorhan Sohaky <lorhansohaky@gmail.com>
2018-11-23 17:57:49 -0200

    Arrumando codificação do arquivo

--- src/config.asm
+++ src/config.asm
@@ -5,11 +5,11 @@ CORPADRAO TEXTEQU %70h                                              ; Cores do j
 
 .DATA
     corAnterior BYTE ?                                              ; Cores do console antes de iniciar o jogo
-    barraDeTitulo BYTE "Ralph Assembles",0                          ; Título da janela
+    barraDeTitulo BYTE "Ralph Assembles",0                          ; T¡tulo da janela
     
     outHandle HANDLE 0
-    scrBuffer CHAR_INFO COLS*ROWS DUP(<<0>, 0Fh>)                   ; Buffer de saída
-    scrSize COORD <COLS, ROWS>                                      ; Tamanho do buffer de saída
-    scrCoord COORD <0, 0>                                           ; A primeira coordenada da buffer de saída
-    scrRect SMALL_RECT <0, 0, COLS - 1, ROWS - 1>                   ; Retângulo
-    cursorInfo CONSOLE_CURSOR_INFO <>                               ; Informações do cursor
+    scrBuffer CHAR_INFO COLS*ROWS DUP(<<0>, 0Fh>)                   ; Buffer de sa¡da
+    scrSize COORD <COLS, ROWS>                                      ; Tamanho do buffer de sa¡da
+    scrCoord COORD <0, 0>                                           ; A primeira coordenada da buffer de sa¡da
+    scrRect SMALL_RECT <0, 0, COLS - 1, ROWS - 1>                   ; Retƒngulo
+    cursorInfo CONSOLE_CURSOR_INFO <>                               ; Informa‡”es do cursor
